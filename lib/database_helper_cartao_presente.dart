b977037274e2e7ad9eccdb033596c4d6ab7ed6af
Lucasadiel <148377907+Lucasadiel@users.noreply.github.com>
2026-07-01 13:53:40 -0300

    Update database_helper_cartao_presente.dart

--- lib/database_helper_cartao_presente.dart
+++ lib/database_helper_cartao_presente.dart
@@ -1,7 +1,9 @@
+//importa o sqlite e o pacote dart pra conseguir rodar em androi de IOS
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 
+//tabela das caracteristicas do de cada objeto
 class CompraCartaoPresente {
   final int? id;
   final String cartao;
@@ -10,6 +12,7 @@ class CompraCartaoPresente {
   final String formaPagamento;
   final String dataCompra; 
 
+  //construtor onde cria e busca cada objeto 
   CompraCartaoPresente({
     this.id,
     required this.cartao,
@@ -19,6 +22,7 @@ class CompraCartaoPresente {
     required this.dataCompra,
   });
 
+  //transforma em map pois o sqlite não lê em dart
   Map<String, dynamic> toMap() {
     return {
       'id': id,
@@ -30,6 +34,7 @@ class CompraCartaoPresente {
     };
   }
 
+  //trasnforma em dart novamente para cantinuar a fazer as outras funçes em dart
   factory CompraCartaoPresente.fromMap(Map<String, dynamic> map) {
     return CompraCartaoPresente(
       id: map['id'],
@@ -42,21 +47,28 @@ class CompraCartaoPresente {
   }
 }
 
+//funções para saber se ja tem conexão com o banco de dados
 class DatabaseHelperCartaoPresente {
+  //guarda a única instância que tem
   static final DatabaseHelperCartaoPresente instance =
   DatabaseHelperCartaoPresente._internal();
   static Database? _database;
 
+  //cria a instancia internamente
   DatabaseHelperCartaoPresente._internal();
 
+  
   factory DatabaseHelperCartaoPresente() => instance;
 
+  //para não abrir outra conexão nova com o banco de dados, utilizar a que já tem
   Future<Database> get database async {
     if (_database != null) return _database!;
     _database = await _initDatabase();
     return _database!;
   }
 
+  
+  //função para utilizar o banco que já existe para não criar outro e utilizar os mesmos dados
   Future<Database> _initDatabase() async {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, 'vasco_cartao_presente.db');
@@ -69,6 +81,7 @@ class DatabaseHelperCartaoPresente {
     );
   }
 
+  //cria a tabela do sql para o uso
   Future<void> _onCreate(Database db, int version) async {
     await db.execute('''
       CREATE TABLE compras_cartao_presente (
@@ -82,11 +95,13 @@ class DatabaseHelperCartaoPresente {
     ''');
   }
 
+  //insere a compra no banco de dados
   Future<int> insertCompra(CompraCartaoPresente compra) async {
     final db = await database;
     return await db.insert('compras_cartao_presente', compra.toMap());
   }
 
+  //lista as comparas no banco de dados
   Future<List<CompraCartaoPresente>> getAllCompras() async {
     final db = await database;
     final maps =
@@ -94,12 +109,14 @@ class DatabaseHelperCartaoPresente {
     return maps.map((m) => CompraCartaoPresente.fromMap(m)).toList();
   }
 
+  //função de deletar a compra da lista
   Future<int> deleteCompra(int id) async {
     final db = await database;
     return await db
         .delete('compras_cartao_presente', where: 'id = ?', whereArgs: [id]);
   }
 
+  //fechar o banco de dados
   Future<void> close() async {
     final db = await database;
     db.close();
