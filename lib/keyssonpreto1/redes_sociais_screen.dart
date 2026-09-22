34fd9c4cbe5297dd89d0c3f47e61f9996f5725bf
keysson5 <krcs5@aluno.ifal.edu.br>
2026-09-22 08:33:01 -0300

    Clean up comments in redes_sociais_screen.dart
    Removed commented sections regarding avatar generation.

--- lib/keyssonpreto1/redes_sociais_screen.dart
+++ lib/keyssonpreto1/redes_sociais_screen.dart
@@ -48,9 +48,6 @@ class _RedesSociaisScreenState extends State<RedesSociaisScreen> {
 
   bool carregando = true;
 
-  // ---------------------------------------------------------------------
-  // Foto de perfil aleatória (API pública DiceBear - lib/keyssonpreto1/api/avatar_api.dart)
-  // ---------------------------------------------------------------------
   late Future<Uint8List> futureAvatar;
 
   @override
@@ -210,13 +207,10 @@ class _RedesSociaisScreenState extends State<RedesSociaisScreen> {
     );
   }
 
-  // ---------------------------------------------------------------------
-  // Avatar de perfil gerado pela API DiceBear via FutureBuilder
-  // ---------------------------------------------------------------------
   Widget avatarPerfil() {
     return GestureDetector(
       onTap: () {
-        // toca no avatar para gerar um novo, sem precisar reabrir o app
+        
         setState(() {
           futureAvatar = AvatarApi().gerarAvatarAleatorio();
         });
